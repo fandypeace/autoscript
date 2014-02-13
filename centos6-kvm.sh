caa1a7dd2c923e61a4818196b2bd96b4956c6d3c
Arie Wijayanto <me@kangarie.com>
2014-02-13 12:23:59 +0700

    Update centos6-kvm.sh

--- centos6-kvm.sh
+++ centos6-kvm.sh
@@ -2,7 +2,7 @@
 
 # initialisasi var
 OS=`uname -m`;
-MYIP=`hostname --ip-address`;
+MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | grep -v '127.0.0.2'`;
 MYIP2="s/xxxxxxxxx/$MYIP/g";
 
 # go to root
